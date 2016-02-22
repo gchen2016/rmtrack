5633aaf9eca9e9b42b0f023cd0a62721767a8662
Michal Cap <cap@agents.fel.cvut.cz>
2016-02-22 16:21:38 -0500

    cleanup

--- src/main/java/cz/agents/map4rt/CommonTime.java
+++
@@ -1,9 +0,0 @@
-package cz.agents.map4rt;
-
-public class CommonTime {
-	static long startedAt = System.currentTimeMillis(); 
-	
-	public static int currentTimeMs() {
-		return (int) (System.currentTimeMillis() - startedAt);
-	}
-}
