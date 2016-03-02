eda7aa17f8084ac415288c74ebba89f83eb3c676
Michal Cap <cap@agents.fel.cvut.cz>
2016-03-02 11:23:49 -0500

    cleanup

--- src/main/java/cz/agents/rmtrack/CommonTime.java
+++
@@ -1,9 +0,0 @@
-package cz.agents.rmtrack;
-
-public class CommonTime {
-	static long startedAt = System.currentTimeMillis(); 
-	
-	public static int currentTimeMs() {
-		return (int) (System.currentTimeMillis() - startedAt);
-	}
-}
